bd728c621ab0ca201f6ecf8da21103419e133737
drithh <adrielalfeus@student.uns.ac.id>
2022-06-20 20:06:06 +0700

    add icons

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,5 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
-    </div>
+<div class="flex min-h-screen items-center pt-6 sm:justify-center sm:pt-0">
+  <div class="mt-6 h-[36rem] w-[24rem] overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+    {{ $slot }}
+  </div>
 </div>
