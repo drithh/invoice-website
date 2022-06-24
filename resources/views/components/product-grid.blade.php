11a9ff3f04cd82349244544dd9fa603f4ffc471e
magezter-stdnt <christopheraaronw@student.uns.ac.id>
2022-06-24 14:21:33 +0700

    update grid list - minus stock percentage

--- resources/views/components/product-grid.blade.php
+++ resources/views/components/product-grid.blade.php
@@ -1,6 +1,6 @@
 <div class="mb-8 px-8">
   {{-- header --}}
-  <div class="mb-16 flex items-center justify-between rounded-xl bg-white py-8 pl-8 pr-16">
+  <div class="mb-10 flex items-center justify-between rounded-xl bg-white py-8 pl-8 pr-16">
     <div class="left w-[11rem] text-sm">
       <div class="text-primary-textgray my-auto flex items-center justify-between rounded-lg bg-[#EFF1F9]">
         <div class="flex h-[35px] w-[50%] cursor-pointer flex-row items-center justify-center rounded-lg">
@@ -18,12 +18,61 @@
     </div>
   </div>
 
-  <div class="mb-16 grid grid-cols-4 gap-8">
+  <div class="flex flex-row flex-wrap justify-between ">
     @foreach ($items as $item)
-      <div class="h-[26rem] w-full rounded-xl bg-white">{{ $item->name }}</div>
+      <div class="card mb-8 flex flex-col items-center justify-between w-[240px] h-[15rem] bg-white m-3 p-5 rounded-lg shadow">
+        <div class="flex-row flex w-[100%] justify-between ">
+          <div class="flex justify-start w-[30%] h-[100%] ">
+            <div class="{{ $loop->index % 3 == 0 ? 'bg-yellow-300' : ($loop->index % 3 == 1 ? 'bg-red-300' : 'bg-cyan-300') }} h-[50px] w-[50px] rounded-xl outline outline-gray-400 outline-3"></div>
+          </div>
+          <div class="w-[65%] h-[100%] ">
+            <h3 class="inline-block text-primary-textdark text-xs font-bold align-middle">{{ $item->name }}</h3>
+          </div>
+        </div>
+        <div class="flex flex-col w-[100%] ">
+          <div class="w-[50%] h-[100%] ">
+            <h3 class="inline-block text-primary-textdark text-xs font-semibold align-middle">Stok</h3>
+          </div>
+          <div class="t">{{ $item->remaining_stock }}</div>
+        </div>
+        <div class="flex flex-col w-[100%] ">
+          <div class="w-[50%] h-[100%] ">
+            <h3 class="inline-block text-primary-textdark text-xs font-semibold align-middle">Harga</h3>
+          </div>
+          <div class="{{ $loop->index % 3 == 0 ? 'text-primary-warmblue' : ($loop->index % 3 == 1 ? 'text-primary-orange' : 'text-primary-warmpink') }}">
+            Rp{{ $item->retail_price }},00
+          </div>
+        </div>
+      </div>
     @endforeach
   </div>
   <div class="rounded-xl bg-white py-8 pl-8 pr-16">
     {{ $items->links() }}
   </div>
 </div>
+
+
+{{-- <div class="px-[30px]">
+      <div class="">
+        <div class="mb-10 flex items-center justify-between rounded-lg bg-white py-[30px] pl-[40px] pr-[60px]">
+          <div class="left w-[11rem] text-sm">
+            <div class="text-primary-textgray my-auto flex items-center justify-between rounded-lg bg-[#EFF1F9]">
+              <div class="flex flex-row h-[35px] w-[50%] items-center justify-center rounded-lg cursor-pointer ">
+                <x-list-icon></x-list-icon>
+                <button
+                  class="flex cursor-pointer items-center justify-center ml-1 "
+                  onclick="listView()">List</button>
+              </div>
+              <div class="flex flex-row h-[35px] w-[50%] items-center justify-center rounded-lg cursor-pointer 
+              bg-primary-cyan text-white">
+                <x-grid-icon></x-grid-icon>
+                <button
+                class="flex h-[35px] w-[50%] items-center justify-center ml-1"
+                onclick="gridView()">Grid</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+        {{ $items->links() }}
+      </div> --}}
