53d8a6924d117e594c47e8f8ad52e58eb4acc0cb
magezter-stdnt <christopheraaronw@student.uns.ac.id>
2022-06-29 20:36:36 +0700

    add edit profile feature

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -76,9 +80,63 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        
+            try {
+                $dataValidation = 
+                $this->validate($request, [
+                        'username' => ['required', 'min:3', 'max:255'],
+                        'email' =>  'required|email|unique:users,email,'.$request->user()->id,
+                        // 'password' => 'required',
+                        'address' => 'nullable',
+                        'phone' => 'nullable',
+                        'nik' => 'nullable|digits:16',
+                        'birthday' => 'nullable|date',
+                ]);
+                    $request->validate([
+                        'password' => 'required',
+                    ]);
+
+                    if (!Hash::check($request->password, $request->user()->password)) {
+                        return response()->json([
+                            'err' => 'Password Salah',
+                        ], 422);
+                    }
+
+                    $request->user()->password = Hash::make($request->password);
+                    $request->user()->save();
+            }
+            catch (ValidationException $exception) {
+                return response()->json([
+                    'status' => 'error',
+                    'msg'    => 'Error',
+                    'errors' => $exception->errors(),
+                ], 422);
+            }
+
+            // return response()->json([
+            //     'data' => $request->user()->id
+            // ]);
+
+            User::where('id', $request->user()->id)->update($dataValidation);
+            
+            return response()->json([
+                'success' => 'Data telah berhasil diupdate'
+            ]);
+
+        // $validatedData = $request->validate($rules);
+
+        // if($request->file('photo')){
+        //     if($request->oldPhoto){
+        //         Storage::delete($request->oldPhoto);
+        //     }
+        //     $validatedData['photo'] = $request->file('photo')->store('users-photo');
+        // }
+
+
+        // User::where('id', $user->id)->update($validatedData);
+        // return ($request);
     }
 
     /**
